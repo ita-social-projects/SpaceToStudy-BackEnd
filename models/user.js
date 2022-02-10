04be67666e6a75eb6f5210ea35924c58c4c4ed6e
Nelia Zbyshko <nelyazbishko@gmail.com>
2022-02-11 01:16:02 +0200

    refactored password encryption

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const { roles: { STUDENT, TEACHER, ADMIN } } = require('../consts/index');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = new Schema(
   {
@@ -24,5 +26,29 @@ const userSchema = new Schema(
     }
   }
 )
+
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+        
+            user.password = hash;
+            next();
+        });
+    });
+});
+     
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model('User', userSchema)

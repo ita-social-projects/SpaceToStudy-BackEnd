21dd8e29808deae26f11f15e22be70b3f91f5e3a
Boris Giga <61072426+boris-giga@users.noreply.github.com>
2022-03-23 10:22:21 +0200

    feature/3/Auth_routes_and_controllers (#5)
    * created basic routes and controllers for auth, added bcrypt for hashing passwords
    
    * added some comments
    
    * created func that generates token which will be sent to client
    
    * refactored, organized code
    
    * cleaned package and package-lock from bcrypt
    
    * moved hashing to controller; some fixes
    
    * added package and package-lock wit bcrypt to commit
    
    * extracted consts; updated login controller and error handler
    
    * took part of Nelia's model; extracted bcrypt logic to controllers/utils; extracted auth err messages to consts/errors; updated errorHandler
    
    * fixed eroor messages and status codes
    
    * added verification if user was already created in signUp controller; fixed inappropriate status codes to return to the client; implemented new error handler
    
    * removed errorHandler
    
    * refactored exports

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const swaggerUI = require('swagger-ui-express')
 
 const swaggerOptions = require('~/swagger-settings')
 const example = require('~/routes/example')
+const auth = require('~/routes/auth')
 
 const app = express()
 
@@ -27,12 +28,13 @@ const swaggerSettings = swaggerJsDoc(swaggerOptions)
 app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerSettings))
 
 app.use('/example', example)
+app.use('/auth', auth)
 
 mongoose
   .connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@teachma.693y8.mongodb.net/test`)
   .then(() => {
     app.listen(process.env.SERVER_PORT, () => {
-      console.log(`Server is running at port ${process.env.SERVER_PORT}`)
+      console.log(`Server is running on port ${process.env.SERVER_PORT}`)
     })
   })
   .catch(err => console.log(err))
---
+++ consts/auth.js
@@ -0,0 +1,15 @@
+const roles = {
+  STUDENT: 'student',
+  MENTOR: 'mentor',
+  ADMIN: 'admin'
+}
+
+const JWT = {
+  MAX_AGE: '24h',
+  SECRET: 'PUTIN HUILO',
+}
+
+module.exports = {
+  roles,
+  JWT
+}

f79bfa3ce7d0f2b4ec38dcf78bf036b1fddfbefc
Nelia Zbyshko <nelyazbishko@gmail.com>
2022-03-28 20:24:18 +0300

    aligned with auth

--- models/user.js
+++ models/user.js
@@ -1,39 +1,73 @@
 const mongoose = require('mongoose')
-const { roles: { STUDENT, MENTOR, ADMIN } } = require('~/consts/auth')
-const { authErr: {ROLE_NOT_SUPPORTED} } = require('~/consts/errors')
-
 const Schema = mongoose.Schema
+const { 
+    roles: { STUDENT, MENTHOR, ADMIN }, 
+    errors: {TO_SHORT_PASSWORD, ROLE_NOT_SUPPORTED} 
+} = require('../consts/index');
+const { numberRegExp } = require('../consts/regexp');  
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = new Schema(
   {
-    role: {
-      type: String,
-      enum: {
-        values: [STUDENT, MENTOR, ADMIN],
-        message: ROLE_NOT_SUPPORTED
-      },
-      required: true,
-      default: STUDENT
-    },
     firstName: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     lastName: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: {
+            values: [STUDENT, MENTHOR, ADMIN],
+            message: ROLE_NOT_SUPPORTED
+        },
+        required: true,
+        default: STUDENT
     },
     email: {
-      type: String,
-      required: [true, 'Please enter an email'],
-      unique: true,
-      lowercase: true,
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
     },
     password: {
-      type: String,
-      required: [true, 'Please enter a password'],
+        type: String,
+        required: true,
+        min: [8, TO_SHORT_PASSWORD]
+    },
+    phoneNumber: {
+        type: String,
+        required: false,
+        validate: {
+            validator: function(v) {
+              return numberRegExp.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+          },
     }
   }
 )
-
+userSchema.pre('save', function(next) {
+    var user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+        
+            user.password = hash;
+            next();
+        });
+    });
+});
+     
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 module.exports = mongoose.model('User', userSchema)

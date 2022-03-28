bde49fb7f846d38f58f7c8c81047a5fcfa635739
Nelia Zbyshko <nelyazbishko@gmail.com>
2022-03-28 20:24:18 +0300

    aligned with auth

--- consts/auth.js
+++ consts/auth.js
@@ -1,15 +1,8 @@
-const roles = {
-  STUDENT: 'student',
-  MENTOR: 'mentor',
-  ADMIN: 'admin'
-}
-
 const JWT = {
   MAX_AGE: '24h',
   SECRET: 'PUTIN HUILO',
 }
 
 module.exports = {
-  roles,
   JWT
 }

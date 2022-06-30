d87b90f4634392ffee8d7447f29cbdea4ca543a7
wonderflu <flupwnzx@gmail.com>
2022-06-30 13:38:45 +0200

    made logger avail only for console (temp solution)

--- logger/logger.js
+++ logger/logger.js
@@ -1,18 +1,19 @@
 const { createLogger, transports, format } = require('winston')
 const { combine, timestamp, json, metadata } = format
-require('winston-mongodb')
+// require('winston-mongodb')
 
 const logger = createLogger({
-  level: 'info',
   format: combine(timestamp(), metadata(), json()),
   transports: [
-    new transports.MongoDB({
-      db: process.env.MONGODB_URL,
-      options: { useUnifiedTopology: true },
-      expireAfterSeconds: 604800,
+    // new transports.MongoDB({
+    //   db: process.env.MONGODB_URL,
+    //   options: { useUnifiedTopology: true },
+    //   expireAfterSeconds: 5,
+    //   handleExceptions: true
+    // }),
+    new transports.Console({
       handleExceptions: true
-    }),
-    new transports.Console()
+    })
   ]
 })
 

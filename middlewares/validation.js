b6c5fcdca7c7e9e77eed947a7d09a82316db30b0
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-07-11 14:36:57 +0300

    refactor

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -3,25 +3,21 @@ const { BODY_IS_NOT_DEFINED } = require('~/consts/errors')
 const { validateRequired, validateType, validateLength } = require('~/utils/validationHelper')
 
 const validationMiddleware = (schema) => {
-  return function validation(req, _res, next) {
-    try {
-      const { body } = req
-      if (!body) {
-        throw createError(422, BODY_IS_NOT_DEFINED)
-      }
-      for (const schemaField of Object.entries(schema)) {
-        const field = body[schemaField[0]]
-        validateRequired(schemaField, field)
-        if (field) {
-          validateType(schemaField, field)
-          validateLength(schemaField, field)
-        }
-      }
-
-      next()
-    } catch (err) {
-      next(err)
+  return (req, _res, next) => {
+    const { body } = req
+    if (!body) {
+      throw createError(422, BODY_IS_NOT_DEFINED)
     }
+    for (const schemaField of Object.entries(schema)) {
+      const reqBodyField = body[schemaField[0]]
+      validateRequired(schemaField, reqBodyField)
+      if (reqBodyField) {
+        validateType(schemaField, reqBodyField)
+        validateLength(schemaField, reqBodyField)
+      }
+    }
+
+    next()
   }
 }
 

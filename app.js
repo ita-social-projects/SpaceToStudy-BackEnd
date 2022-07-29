ad6a1c16c8251c97aef74e1eb8fe60cbfcc3d10e
wonderflu <flupwnzx@gmail.com>
2022-07-29 10:19:14 +0200

    removed extra code and space

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-
 const swaggerJsDoc = require('swagger-jsdoc')
 const swaggerUI = require('swagger-ui-express')
 

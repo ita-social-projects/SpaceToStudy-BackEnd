1acaf391e68c68740a02e6b5d679650e0537058f
Polina Balazhy <71522782+High-Voltaged@users.noreply.github.com>
2022-08-18 12:36:16 +0300

    Modify login validation schema (#75)

--- validation/schemas/login.js
+++ validation/schemas/login.js
@@ -1,22 +1,11 @@
-const {
-  lengths: { MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH },
-  regex: { EMAIL_PATTERN, PASSWORD_PATTERN }
-} = require('~/consts/validation')
-
 const loginValidationSchema = {
   email: {
     type: 'string',
-    required: true,
-    regex: EMAIL_PATTERN
+    required: true
   },
   password: {
     type: 'string',
-    required: true,
-    length: {
-      min: MIN_PASSWORD_LENGTH,
-      max: MAX_PASSWORD_LENGTH
-    },
-    regex: PASSWORD_PATTERN
+    required: true
   }
 }
 

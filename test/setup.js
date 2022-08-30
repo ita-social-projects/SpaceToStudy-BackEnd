3194cc2709f1ec5763664788536cbb0b75c446b4
Polina Balazhy <lefugdot@gmail.com>
2022-08-30 15:20:14 +0300

    Add tests for signup, login, confirm

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,3 @@
-const { existsSync } = require('fs')
 const express = require('express')
 const mongoose = require('mongoose')
 const request = require('supertest')
@@ -13,20 +12,8 @@ const serverInit = async () => {
 }
 
 const serverCleanup = async (server) => {
-  if (existsSync('.env.test.local')) {
-    await dropAllCollections()
-  }
   await mongoose.connection.close()
   await server.close()
-}
-
-const dropAllCollections = async () => {
-  const collections = await mongoose.connection.db.collections()
-  const areDropped = []
-  collections.forEach((collection) => {
-    areDropped.push(collection.drop())
-  })
-  await Promise.all(areDropped)
 }
 
 module.exports = { serverInit, serverCleanup }

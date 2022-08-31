dc378f8cf5f9fe82c5b7a3ff9d9496fd14a6e91e
Polina Balazhy <lefugdot@gmail.com>
2022-08-31 18:57:55 +0300

    Drop collections of a local DB before tests

--- initialization/database.js
+++ initialization/database.js
@@ -5,8 +5,24 @@ const {
 } = require('~/configs/config')
 const logger = require('~/logger/logger')
 
+const dropAllCollections = async () => {
+  const collections = await mongoose.connection.db.collections()
+  const areDropped = []
+  collections.forEach((collection) => {
+    areDropped.push(collection.drop())
+  })
+  await Promise.all(areDropped)
+}
+
+const checkForLocalDB = async () => {
+  if (process.env.NODE_ENV === 'test') {
+    await dropAllCollections()
+  }
+}
+
 const databaseInitialization = async () => {
   await mongoose.connect(MONGODB_URL)
+  await checkForLocalDB()
   logger.info('Connected to MongoDB.')
 }
 

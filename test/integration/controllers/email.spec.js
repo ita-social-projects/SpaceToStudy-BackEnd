fd46ac845066f29d571331b7484c093c3c0cb303
Yevhen Dzyuba <evgendzuba7@gmail.com>
2022-09-07 14:03:47 +0300

    add tests for email controller (#110)

---
+++ test/integration/controllers/email.spec.js
@@ -0,0 +1,31 @@
+const { serverInit, serverCleanup } = require('~/test/setup')
+const { expectError } = require('~/test/helpers')
+const errors = require('~/consts/errors')
+const emailSubject = require('~/consts/emailSubject')
+
+describe('Email controller', () => {
+  let app, server
+
+  beforeAll(async () => {
+    ;({ app, server } = await serverInit())
+  })
+
+  afterAll(async () => {
+    await serverCleanup(server)
+  })
+
+  describe('Send email endpoint', () => {
+    it('should send email', async () => {
+      const response = await app
+        .post('/send-email')
+        .send({ email: 'test@gmail.com', subject: emailSubject.EMAIL_CONFIRMATION })
+
+      expect(response.statusCode).toBe(204)
+    })
+    it('should throw TEMPLATE_NOT_FOUND error', async () => {
+      const response = await app.post('/send-email').send({ email: 'test@gmail.com', subject: 'incorrect template'})
+
+      expectError(404, errors.TEMPLATE_NOT_FOUND, response)
+    })
+  })
+})

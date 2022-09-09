0ea957c9ef662abd319c0fafb5b24e292f9a2078
Polina Balazhy <lefugdot@gmail.com>
2022-09-10 00:47:09 +0300

    Add tests for auth & error middleware

---
+++ test/unit/middlewares/auth.spec.js
@@ -0,0 +1,54 @@
+require('~/initialization/envSetup')
+const authMiddleware = require('~/middlewares/auth')
+const { createUnauthorizedError } = require('~/utils/errorsHelper')
+const tokenService = require('~/services/token')
+
+describe('Auth middleware', () => {
+  const error = createUnauthorizedError()
+  const mockResponse = {}
+  const mockNextFunc = jest.fn()
+
+  it("Should throw UNAUTHORIZED error when auth header isn't given", () => {
+    const mockRequest = {
+      headers: {}
+    }
+    const middlewareFunc = () => authMiddleware(mockRequest, mockResponse, mockNextFunc)
+
+    expect(middlewareFunc).toThrow(error)
+  })
+
+  it("Should throw UNAUTHORIZED error when access token isn't given", () => {
+    const mockRequest = {
+      headers: {
+        authorization: 'invalid_token'
+      }
+    }
+    const middlewareFunc = () => authMiddleware(mockRequest, mockResponse, mockNextFunc)
+
+    expect(middlewareFunc).toThrow(error)
+  })
+
+  it('Should throw UNAUTHORIZED error when access token is invalid', () => {
+    const mockRequest = {
+      headers: {
+        authorization: 'Bearer token'
+      }
+    }
+    const middlewareFunc = () => authMiddleware(mockRequest, mockResponse, mockNextFunc)
+
+    expect(middlewareFunc).toThrow(error)
+  })
+
+  it('Should save userData from accessToken to a request object', () => {
+    const payload = { userId: 'testId' }
+    const { accessToken } = tokenService.generateTokens(payload)
+    const mockRequest = {
+      headers: {
+        authorization: `Bearer ${accessToken}`
+      }
+    }
+    authMiddleware(mockRequest, mockResponse, mockNextFunc)
+
+    expect(mockRequest.user).toEqual(expect.objectContaining(payload))
+  })
+})
---
+++ test/unit/middlewares/error.spec.js
@@ -0,0 +1,30 @@
+const { NOT_FOUND, INTERNAL_SERVER_ERROR } = require('~/consts/errors')
+const errorMiddleware = require('~/middlewares/error')
+const { createError } = require('~/utils/errorsHelper')
+
+describe('Error middleware', () => {
+  const jsonFunc = jest.fn()
+  let mockResponse = {
+    status: jest.fn((_status) => ({
+      json: jsonFunc
+    }))
+  }
+  const mockRequest = {}
+  const mockNextFunc = jest.fn()
+
+  it('Should return the given error status & message', () => {
+    const errorToTest = createError(404, NOT_FOUND)
+    const { status, code, message } = errorToTest
+    errorMiddleware(errorToTest, mockRequest, mockResponse, mockNextFunc)
+
+    expect(jsonFunc).toBeCalledWith({ status, code, message })
+  })
+
+  it('Should return the given INTERNAL_SERVER_ERROR if no error was given', () => {
+    const message = 'Server-specific error message.'
+    const { status, code } = createError(500, { message, ...INTERNAL_SERVER_ERROR })
+    errorMiddleware({ message }, mockRequest, mockResponse, mockNextFunc)
+
+    expect(jsonFunc).toBeCalledWith({ status, code, message })
+  })
+})

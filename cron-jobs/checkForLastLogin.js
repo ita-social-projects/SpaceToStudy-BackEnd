62fd33995bdc167f40cc5a354573383d7bd59938
Polina Balazhy <lefugdot@gmail.com>
2022-09-10 23:37:25 +0300

    fix cronjob's email language

--- cron-jobs/checkForLastLogin.js
+++ cron-jobs/checkForLastLogin.js
@@ -3,6 +3,9 @@ const userService = require('~/services/user')
 const emailService = require('~/services/email')
 const emailSubject = require('~/consts/emailSubject')
 const { oneDayInMs } = require('~/consts/auth')
+const {
+  enums: { LANG_ENUM }
+} = require('~/consts/validation')
 
 const checkUsersForLastLogin = new CronJob('00 00 03 * * *', () => checkLastLogin())
 
@@ -11,13 +14,14 @@ const checkLastLogin = async () => {
   const dateNow = new Date()
   const daysToSendEmail = 173
   const daysToDeleteUser = 180
+  const language = LANG_ENUM[0]
 
   users.forEach(async (user) => {
     const { email, firstName, lastLogin, _id } = user
     if (lastLogin) {
       const differenceInDays = Math.floor((dateNow.getTime() - lastLogin.getTime()) / oneDayInMs)
       if (differenceInDays === daysToSendEmail) {
-        await emailService.sendEmail(email, emailSubject.LONG_TIME_WITHOUT_LOGIN, { email, firstName })
+        await emailService.sendEmail(email, emailSubject.LONG_TIME_WITHOUT_LOGIN, language, { email, firstName })
       }
       if (differenceInDays >= daysToDeleteUser) {
         await userService.deleteUser(_id)

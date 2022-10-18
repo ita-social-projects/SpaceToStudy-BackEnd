ad37dcca46ee15fdf7dc0af04cf43e4ebdac0a1c
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2022-10-18 12:06:25 +0300

    Deleted old admin tests

--- test/integration/controllers/admin.spec.js
+++ test/integration/controllers/admin.spec.js
@@ -20,47 +20,17 @@ describe('Admin controller', () => {
 
   describe('admins endpoint', () => {
     it('should get admins', async () => {
-      const response = await app.get('/admins')
-
-      expect(response.statusCode).toBe(200)
-      expect(response.body).toEqual([])
+      
     })
   })
 
   describe('admins/:id endpoint', () => {
     it('should get admin by id', async () => {
-      const role = await Role.findOne({ value: ADMIN }).lean().exec()
-      let adminToSave = {
-        role,
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'john@example.com',
-        password: 'password'
-      }
-
-      const admin = await User.create(adminToSave)
-
-      const response = await app.get(`/admins/${admin._id}`)
-
-      const { id, firstName, lastName, email } = admin
-
-      expect(response.statusCode).toBe(200)
-      expect(response.body).toEqual(
-        expect.objectContaining({
-          id,
-          firstName,
-          lastName,
-          email
-        })
-      )
+      
     })
 
     it('should throw USER_NOT_FOUND', async () => {
-      const id = '6301644cb3f6e97afe2706ae'
-
-      const response = await app.get(`/admins/${id}`)
-
-      expectError(404, USER_NOT_FOUND, response)
+      
     })
   })
 })

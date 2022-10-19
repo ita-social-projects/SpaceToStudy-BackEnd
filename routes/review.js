e97ab43364e9987dc0917309c5f6f43abee6428c
tarasSanotskyi <badsanta2102@gmail.com>
2022-10-19 10:44:41 +0300

    add controller

--- routes/review.js
+++ routes/review.js
@@ -1,15 +1,17 @@
 const express = require('express')
 
 const idValidation = require('~/middlewares/idValidation')
+const asyncWrapper = require('~/middlewares/asyncWrapper')
+const reviewController = require('~/controllers/review')
 
 const router = express.Router()
 
 router.param('reviewId', idValidation)
 
-router.get('/')
-router.post('/')
-router.get('/:reviewId')
-router.patch('/:reviewId')
-router.delete('/:reviewId')
+router.get('/', asyncWrapper(reviewController.getReviews))
+router.post('/', asyncWrapper(reviewController.addReview))
+router.get('/:reviewId', asyncWrapper(reviewController.getReviewById))
+router.patch('/:reviewId', asyncWrapper(reviewController.updateReview))
+router.delete('/:reviewId', asyncWrapper(reviewController.deleteReview))
 
 module.exports = router

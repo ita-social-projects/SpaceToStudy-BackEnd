c92437f3f70ad7aaca1944b301d3eeec243b00b9
Sarakhman Anatoliy <63456632+Tolik170@users.noreply.github.com>
2023-01-13 17:36:16 +0200

    added subject model, route and swagger (#167)
    * added subject model, route and swagger
    
    * fix
    
    * changed proficiency level enum and made some changes
    
    * fix levels enum

---
+++ models/subject.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose')
+const {
+  enums: { SUBJECT_LEVEL_ENUM }
+} = require('~/consts/validation')
+
+const subjectSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Please, enter a subject name']
+  },
+  price: {
+    type: Number,
+    min: [0, 'Price should be greater than 0'],
+    required: [true, 'Please, enter a subject price']
+  },
+  proficiencyLevel: {
+    type: String,
+    enum: SUBJECT_LEVEL_ENUM,
+    required: [true, 'Please, choose a subject proficiency level']
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: [true, 'Please, choose a subject category']
+  }
+})
+
+module.exports = model('Subject', subjectSchema)
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const user = require('~/routes/user')
 const student = require('~/routes/student')
 const email = require('~/routes/email')
 const review = require('~/routes/review')
+const subject = require('~/routes/subject')
 const tutor = require('~/routes/tutor')
 
 const router = express.Router()
@@ -16,6 +17,7 @@ router.use('/students', student)
 router.use('/admins', admin)
 router.use('/send-email', email)
 router.use('/reviews', review)
+router.use('/subjects', subject)
 router.use('/tutors', tutor)
 
 module.exports = router

b29be0fd54f966257534f8ed81bebae32549a137
Roman Ortynskyi <50491616+romanortynskyi@users.noreply.github.com>
2023-01-25 09:50:22 +0200

    Implemented admin (#174)
    * Implemented admin
    
    * Wrote test for admin
    
    * Wrote test for the admin invitation controller
    
    Co-authored-by: Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const email = require('~/routes/email')
 const review = require('~/routes/review')
 const subject = require('~/routes/subject')
 const tutor = require('~/routes/tutor')
+const adminInvitation = require('~/routes/adminInvitation')
 
 const router = express.Router()
 
@@ -19,5 +20,6 @@ router.use('/send-email', email)
 router.use('/reviews', review)
 router.use('/subjects', subject)
 router.use('/tutors', tutor)
+router.use('/admin-invitations', adminInvitation)
 
 module.exports = router
--- services/admin.js
+++ services/admin.js
@@ -1,43 +1,166 @@
-const User = require('~/models/user')
-const Role = require('~/models/role')
+const Admin = require('~/models/admin')
 const {
-  roles: { ADMIN }
-} = require('~/consts/auth')
-const { USER_NOT_FOUND } = require('~/consts/errors')
+  ALREADY_REGISTERED,
+  USER_NOT_FOUND,
+  USER_ALREADY_BLOCKED,
+  USER_ALREADY_UNBLOCKED
+} = require('~/consts/errors')
 const { createError } = require('~/utils/errorsHelper')
+const { hashPassword } = require('~/utils/passwordHelper')
 
 const adminService = {
-  getAdmins: async () => {
-    const role = await Role.findOne({ value: ADMIN }).lean().exec()
-    const admins = await User.find({
-      role: role._id
-    })
-      .lean()
-      .exec()
+  createAdmin: async (role, firstName, lastName, email, password, language) => {
+    const admin = await Admin.findOne({ email }).lean().exec()
 
-    return admins.map(({ _id, firstName, lastName, email }) => ({
-      id: _id,
+    if (admin) {
+      throw createError(409, ALREADY_REGISTERED)
+    }
+
+    const hashedPassword = await hashPassword(password)
+
+    const newAdmin = await Admin.create({
+      role,
       firstName,
       lastName,
-      email
-    }))
+      email,
+      password: hashedPassword,
+      language
+    })
+
+    return newAdmin
   },
 
-  getAdmin: async (userId) => {
-    const role = await Role.findOne({ value: ADMIN }).lean().exec()
-    const admin = await User.findOne({
-      _id: userId,
-      role: role._id
-    })
-      .lean()
+  getAdmins: async ({
+    skip = 0,
+    limit = 10,
+    name,
+    email,
+    active,
+    blocked,
+    signUpDateFrom,
+    signUpDateTo,
+    lastLoginFrom,
+    lastLoginTo,
+    sortByName,
+    sortByEmail,
+    sortByLastLogin,
+    sortBySignUpDate
+  }) => {
+    const match = {
+      name: {
+        $regex: name.length > 0 ? name : '.*',
+        $options: 'i'
+      },
+      email: {
+        $regex: email.length > 0 ? email : '.*',
+        $options: 'i'
+      },
+      active,
+      blocked,
+      signUpDate: {
+        $gte: signUpDateFrom,
+        $lte: signUpDateTo
+      },
+      lastLogin: {
+        $gte: lastLoginFrom,
+        $lte: lastLoginTo
+      }
+    }
+
+    const sort = {
+      name: sortByName,
+      email: sortByEmail,
+      lastLogin: sortByLastLogin,
+      signUpDate: sortBySignUpDate
+    }
+
+    const [admins] = await Admin.aggregate([
+      {
+        $addFields: {
+          name: { $concat: ['$firstName', ' ', '$lastName'] }
+        }
+      },
+      {
+        $project: {
+          firstName: 0,
+          lastName: 0
+        }
+      }
+    ])
+      .facet({
+        items: [{ $match: match }, { $sort: sort }, { $skip: skip }, { $limit: limit }],
+        calculations: [{ $match: match }, { $count: 'count' }]
+      })
       .exec()
+
+    return {
+      items: admins.items,
+      count: admins.calculations[0]?.count || 0
+    }
+  },
+
+  getAdminById: async (id) => {
+    const admin = await Admin.findById(id).lean().exec()
 
     if (!admin) {
       throw createError(404, USER_NOT_FOUND)
     }
 
-    const { _id: id, firstName, lastName, email, isEmailConfirmed } = admin
-    return { id, firstName, lastName, email, isEmailConfirmed }
+    return admin
+  },
+
+  updateAdmin: async (id, updateData) => {
+    const admin = await Admin.findByIdAndUpdate(id, updateData, { new: true }).lean().exec()
+
+    if (!admin) {
+      throw createError(404, USER_NOT_FOUND)
+    }
+
+    return admin
+  },
+
+  blockAdmin: async (id) => {
+    const admin = await Admin.findById(id).exec()
+
+    if (!admin) {
+      throw createError(404, USER_NOT_FOUND)
+    }
+
+    if (admin.blocked) {
+      throw createError(409, USER_ALREADY_BLOCKED)
+    }
+
+    admin.blocked = true
+    await admin.save()
+
+    return admin
+  },
+
+  unblockAdmin: async (id) => {
+    const admin = await Admin.findById(id).exec()
+
+    if (!admin) {
+      throw createError(404, USER_NOT_FOUND)
+    }
+
+    if (!admin.blocked) {
+      throw createError(409, USER_ALREADY_UNBLOCKED)
+    }
+
+    admin.blocked = false
+    await admin.save()
+
+    return admin
+  },
+
+  deleteAdmin: async (id) => {
+    const admin = await Admin.findById(id).exec()
+
+    if (!admin) {
+      throw createError(404, USER_NOT_FOUND)
+    }
+
+    await admin.delete()
   }
 }
 

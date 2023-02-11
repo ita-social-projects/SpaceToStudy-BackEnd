8d32d96c33e080d008b837c4a3ce85704c2af548
MickhaelSh <gmerdok@gmail.com>
2023-02-11 10:13:29 +0200

    refactored auth, removed tutor and student models

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,17 @@ const express = require('express')
 const admin = require('~/routes/admin')
 const auth = require('~/routes/auth')
 const user = require('~/routes/user')
-const student = require('~/routes/student')
 const email = require('~/routes/email')
 const review = require('~/routes/review')
 const subject = require('~/routes/subject')
-const tutor = require('~/routes/tutor')
 
 const router = express.Router()
 
 router.use('/auth', auth)
 router.use('/users', user)
-router.use('/students', student)
 router.use('/admins', admin)
 router.use('/send-email', email)
 router.use('/reviews', review)
 router.use('/subjects', subject)
-router.use('/tutors', tutor)
 
 module.exports = router

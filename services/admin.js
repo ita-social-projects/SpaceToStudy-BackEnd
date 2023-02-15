03a137d26537826bd4646ee6750d6936b471d12b
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2023-02-15 16:07:47 +0200

    Fixed getAdmins

--- services/admin.js
+++ services/admin.js
@@ -60,10 +60,17 @@ const adminService = {
         $gte: createdAtFrom,
         $lte: createdAtTo
       },
-      lastLogin: {
-        $gte: lastLoginFrom,
-        $lte: lastLoginTo
-      }
+      $or: [
+        {
+          lastLogin: null
+        },
+        {
+          lastLogin: {
+            $gte: lastLoginFrom,
+            $lte: lastLoginTo
+          }
+        }
+      ]
     }
 
     const sort = {

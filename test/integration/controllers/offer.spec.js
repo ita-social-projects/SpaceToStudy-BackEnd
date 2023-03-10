940a36d12a5f7d3fb26b9a257e82e3d0a6e62f6e
Vladyslav Shkil <41526202+greentoss@users.noreply.github.com>
2023-03-10 12:14:38 +0300

    added tests for offer entity controller (#217)
    * added tests for offer entity controller
    
    * used utils/testUserAuth
    
    * refactored test
    
    * refactored

---
+++ test/integration/controllers/offer.spec.js
@@ -0,0 +1,119 @@
+const { serverCleanup, serverInit } = require('~/test/setup')
+const { expectError } = require('~/test/helpers')
+const { OFFER_NOT_FOUND } = require('~/consts/errors')
+const testUserAuthentication = require('~/utils/testUserAuth')
+
+const endpointUrl = '/offers/'
+const nonExistingOfferId = '6329a45601bd35b5fff1cf8c'
+
+let testOffer = {
+  price: 330,
+  proficiencyLevel: 'Beginner',
+  description: 'TEST 123ASD',
+  languages: ['Ukrainian'],
+  subjectId: '63da8767c9ad4c9a0b0eacd3',
+  categoryId: '63525e23bf163f5ea609ff2b'
+}
+
+const updateData = {
+  price: 555
+}
+
+describe('Offer controller', () => {
+  let app, server, accessToken, testOfferResponse
+
+  beforeEach(async () => {
+    ;({ app, server } = await serverInit())
+    accessToken = await testUserAuthentication(app)
+    testOfferResponse = await app.post(endpointUrl).set('Authorization', `Bearer ${accessToken}`).send(testOffer)
+
+    testOffer = testOfferResponse.body
+  })
+
+  afterEach(async () => {
+    await serverCleanup(server)
+  })
+
+  describe(`test POST ${endpointUrl}`, () => {
+    it('should create new offer', async () => {
+      const { _id, userId } = testOfferResponse.body
+
+      expect(testOfferResponse.statusCode).toBe(201)
+      expect(testOfferResponse.body).toEqual(
+        expect.objectContaining({
+          _id,
+          price: 330,
+          proficiencyLevel: 'Beginner',
+          description: 'TEST 123ASD',
+          languages: ['Ukrainian'],
+          authorRole: 'student',
+          userId,
+          subjectId: '63da8767c9ad4c9a0b0eacd3',
+          categoryId: '63525e23bf163f5ea609ff2b',
+          isActive: false,
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String)
+        })
+      )
+    })
+  })
+
+  describe(`test GET ${endpointUrl}`, () => {
+    it('should GET all offers', async () => {
+      const response = await app.get(endpointUrl).set('Authorization', `Bearer ${accessToken}`)
+
+      expect(response.statusCode).toBe(200)
+      expect(Array.isArray(response.body)).toBeTruthy()
+      expect(response.body[0]).toEqual(expect.objectContaining(testOffer))
+    })
+  })
+
+  describe(`test GET ${endpointUrl}:id`, () => {
+    it('should get an offer by ID', async () => {
+      const response = await app.get(endpointUrl + testOffer._id).set('Authorization', `Bearer ${accessToken}`)
+
+      expect(response.body).toEqual(expect.objectContaining(testOffer))
+      expect(response.statusCode).toBe(200)
+    })
+
+    it('should throw OFFER_NOT_FOUND', async () => {
+      const response = await app.get(endpointUrl + nonExistingOfferId).set('Authorization', `Bearer ${accessToken}`)
+
+      expectError(404, OFFER_NOT_FOUND, response)
+    })
+  })
+
+  describe(`test UPDATE ${endpointUrl}:id`, () => {
+    it('should update offer by ID', async () => {
+      const response = await app
+        .patch(endpointUrl + testOffer._id)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(updateData)
+
+      expect(response.statusCode).toBe(204)
+    })
+
+    it('should throw OFFER_NOT_FOUND', async () => {
+      const response = await app
+        .patch(endpointUrl + nonExistingOfferId)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(updateData)
+
+      expectError(404, OFFER_NOT_FOUND, response)
+    })
+  })
+
+  describe(`test DELETE ${endpointUrl}:id`, () => {
+    it('should delete offer by ID', async () => {
+      const response = await app.delete(endpointUrl + testOffer._id).set('Authorization', `Bearer ${accessToken}`)
+
+      expect(response.statusCode).toBe(204)
+    })
+
+    it('should throw OFFER_NOT_FOUND', async () => {
+      const response = await app.get(endpointUrl + nonExistingOfferId).set('Authorization', `Bearer ${accessToken}`)
+
+      expectError(404, OFFER_NOT_FOUND, response)
+    })
+  })
+})

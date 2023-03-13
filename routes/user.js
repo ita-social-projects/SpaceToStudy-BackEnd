e1b4297799cc17eb4bce124bc7353f48230687f9
mxrcury <demetrgonchar@gmail.com>
2023-03-13 12:49:33 +0200

    fix

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express')
 
 const idValidation = require('~/middlewares/idValidation')
@@ -24,3 +25,24 @@ router.patch('/:id', asyncWrapper(userController.updateUser))
 router.delete('/:id', asyncWrapper(userController.deleteUser))
 
 module.exports = router
+=======
+const express = require('express')
+
+const idValidation = require('~/middlewares/idValidation')
+const asyncWrapper = require('~/middlewares/asyncWrapper')
+const userController = require('~/controllers/user')
+const reviewRouter = require('~/routes/review')
+
+const router = express.Router()
+
+router.param('id', idValidation)
+
+router.use('/:id/:role/reviews', reviewRouter)
+
+router.get('/', asyncWrapper(userController.getUsers))
+router.get('/:id/:role', asyncWrapper(userController.getOneUser))
+router.patch('/:id', asyncWrapper(userController.updateUser))
+router.delete('/:id', asyncWrapper(userController.deleteUser))
+
+module.exports = router
+>>>>>>> 2ba8708 (fix)

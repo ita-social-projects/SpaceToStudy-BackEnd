c6d877a67c387a38c47edf885257a809df8ec2ce
mxrcury <demetrgonchar@gmail.com>
2023-03-14 18:26:35 +0200

    refactor by deleting filtering for nested fields

--- utils/users/filterAllowedUserFields.js
+++ utils/users/filterAllowedUserFields.js
@@ -1,27 +1,9 @@
 const { allowedUserFieldsForUpdate } = require('~/validation/services/user')
 
-const isObjectEmpty = (obj) => Object.keys(obj).length === 0
-
-const isAllowedField = (key, allowedFields = {}) => {
-  if (isObjectEmpty(allowedFields)) return key in allowedUserFieldsForUpdate
-  return key in allowedFields
-}
-const isFieldObject = (key) => typeof key == 'object'
-
-const operateNestedField = ({ key, fields, updatedFields }) => {
-  const nestedFields = fields[key]
-  for (const nestedKey in nestedFields) {
-    if (!updatedFields[key]) updatedFields[key] = {}
-    if (!isAllowedField(nestedKey, allowedUserFieldsForUpdate[key])) continue
-
-    updatedFields[key][nestedKey] = nestedFields[nestedKey]
-  }
-}
-
 const operateField = ({ key, fields, updatedFields }) => {
-  if (isAllowedField(key)) {
-    if (isFieldObject(fields[key])) operateNestedField({ key, fields, updatedFields })
-    else updatedFields[key] = fields[key]
+  const isAllowedField = key in allowedUserFieldsForUpdate
+  if (isAllowedField) {
+    updatedFields[key] = fields[key]
   }
 }
 const filterAllowedUserFields = (fields) => {

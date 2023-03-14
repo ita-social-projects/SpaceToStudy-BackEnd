21dbc3d423453cce9a702f8fccda5420a081bfbe
mxrcury <demetrgonchar@gmail.com>
2023-03-14 22:05:53 +0200

    refactored

--- utils/users/filterAllowedUserFields.js
+++ utils/users/filterAllowedUserFields.js
@@ -1,15 +1,11 @@
 const { allowedUserFieldsForUpdate } = require('~/validation/services/user')
 
-const operateField = ({ key, fields, updatedFields }) => {
-  const isAllowedField = key in allowedUserFieldsForUpdate
-  if (isAllowedField) {
-    updatedFields[key] = fields[key]
-  }
-}
 const filterAllowedUserFields = (fields) => {
   const updatedFields = {}
   for (const key in fields) {
-    operateField({ key, fields, updatedFields })
+    if (key in allowedUserFieldsForUpdate) {
+      updatedFields[key] = fields[key]
+    }
   }
   return updatedFields
 }

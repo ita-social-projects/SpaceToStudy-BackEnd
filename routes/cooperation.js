6cecf06c5ff4789179fca83e07a871c999a28545
Dima Honchar <90138904+mxrcury@users.noreply.github.com>
2023-03-27 16:44:26 +0300

    hot fix (#288)

--- routes/cooperation.js
+++ routes/cooperation.js
@@ -4,7 +4,7 @@ const idValidation = require('~/middlewares/idValidation')
 const asyncWrapper = require('~/middlewares/asyncWrapper')
 const { authMiddleware } = require('~/middlewares/auth')
 const cooperationController = require('~/controllers/cooperation')
-const setCurrentUserId = require('~/middlewares/setCurrentUserId')
+const setCurrentUserIdAndRole = require('~/middlewares/setCurrentUserIdAndRole')
 
 const router = express.Router()
 
@@ -13,7 +13,7 @@ router.use(authMiddleware)
 router.param('id', idValidation)
 
 router.get('/', asyncWrapper(cooperationController.getCooperations))
-router.post('/', setCurrentUserId, asyncWrapper(cooperationController.createCooperation))
+router.post('/', setCurrentUserIdAndRole, asyncWrapper(cooperationController.createCooperation))
 router.get('/:id', asyncWrapper(cooperationController.getCooperationById))
 router.patch('/:id', asyncWrapper(cooperationController.updateCooperation))
 

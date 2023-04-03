01eeb9e57f66169153fee1ac89a23eb9fdabd2ff
Pavlo Diakun <rubber883@gmail.com>
2023-04-03 21:32:58 +0300

    Refactoring + adding of util

--- services/category.js
+++ services/category.js
@@ -1,8 +1,6 @@
 const Category = require('~/models/category')
 const Offer = require('~/models/offer')
-const { createError } = require('~/utils/errorsHelper')
-const { CATEGORY_NOT_FOUND, OFFER_NOT_FOUND } = require('~/consts/errors')
-const mongoose = require('mongoose')
+const conditionCreator = require('~/utils/categories/conditionCreator')
 
 const categoryService = {
   getCategories: async (searchFilter, skip, limit) => {
@@ -20,31 +18,10 @@ const categoryService = {
   },
 
   priceMinMax: async (searchParams) => {
-    const condition = (data) => {
-      if (!data.catid && !data.subid) {
-        return { authorRole: searchParams.authorRole }
-      } else if (!data.subid) {
-        return {
-          authorRole: searchParams.authorRole,
-          categoryId: mongoose.Types.ObjectId(searchParams.catid)
-        }
-      } else if (!data.catid) {
-        return { authorRole: searchParams.authorRole, subjectId: mongoose.Types.ObjectId(searchParams.subid) }
-      } else if (!searchParams) {
-        throw createError(404, OFFER_NOT_FOUND)
-      } else {
-        return {
-          authorRole: searchParams.authorRole,
-          categoryId: mongoose.Types.ObjectId(searchParams.catid),
-          subjectId: mongoose.Types.ObjectId(searchParams.subid)
-        }
-      }
-    }
-
-    const usedCondition = condition(searchParams)
+    const matchCondition = conditionCreator.condition(searchParams)
 
     const minMaxPrices = await Offer.aggregate([
-      { $match: usedCondition },
+      { $match: matchCondition },
       {
         $group: {
           _id: null,
---
+++ utils/categories/conditionCreator.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const condition = (data) => {
+  console.log(data)
+  const condition = {}
+
+  if (data.catid) {
+    condition.categoryId = mongoose.Types.ObjectId(data.catid)
+  }
+
+  if (data.subid) {
+    condition.subjectId = mongoose.Types.ObjectId(data.subid)
+  }
+
+  if (data.authorRole) {
+    condition.authorRole = data.authorRole
+  }
+
+  return condition
+}
+
+module.exports = {
+  condition
+}

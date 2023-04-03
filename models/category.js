4405296af40318d57feb5ad2f0696539efdf9202
mxrcury <demetrgonchar@gmail.com>
2023-04-03 05:57:40 +0300

    added comments to required fields

--- models/category.js
+++ models/category.js
@@ -6,22 +6,21 @@ const categorySchema = new Schema(
     name: {
       type: String,
       unique: true,
-      required: true
+      required: [true, 'User role must be selected.']
     },
     categoryIcon: {
       path: {
         type: String,
-        required: true
+        required: [true, 'User role must be selected.']
       },
       color: {
         type: String,
-        required: true
+        required: [true, 'User role must be selected.']
       }
     },
     totalOffers: {
       type: Number,
       ref: OFFER,
-      required: true,
       default: 0
     }
   },

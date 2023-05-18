242ac7112848393cb4a1327fd756a5c1fd0e195c
Dima Honchar <90138904+mxrcury@users.noreply.github.com>
2023-05-18 19:37:51 +0300

    Added capitalizing letters for category in seeds (#356)
    * added prehook for checking first letter
    
    * remove hooks in case of no existing endpoint for category creating yeat

--- consts/categories.js
+++ consts/categories.js
@@ -1,63 +1,63 @@
 const categories = {
   LANGUAGES: {
-    name: 'languages',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Languages',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   MATHEMATICS: {
-    name: 'mathematics',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Mathematics',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   COMPUTER_SCIENCE: {
-    name: 'computer science',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Computer science',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   MUSIC: {
-    name: 'music',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Music',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   DESIGN: {
-    name: 'design',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Design',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   HISTORY: {
-    name: 'history',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'History',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   BIOLOGY: {
-    name: 'biology',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Biology',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   PAINTING: {
-    name: 'painting',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Painting',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   FINANCES: {
-    name: 'finances',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Finances',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   MARKETING: {
-    name: 'marketing',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Marketing',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   AUDIT: {
-    name: 'audit',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Audit',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   CHEMISTRY: {
-    name: 'chemistry',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Chemistry',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   ASTRONOMY: {
-    name: 'astronomy',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Astronomy',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   PHYSICS: {
-    name: 'physics',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Physics',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   },
   PSYCHOLOGY: {
-    name: 'psychology',
-    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C'}
+    name: 'Psychology',
+    categoryIcon: { path: 'mocked-path-to-icon', color: '#66C42C' }
   }
 }
 

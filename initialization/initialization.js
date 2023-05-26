cff2b5202a6efdcbabf5d2b92e0ad15d5c7ce3e0
Yura Didenko <didenkoyura225@gmail.com>
2023-05-26 11:37:00 +0300

    Fixed sorting subjects and categories. Limit increased for JSON

--- initialization/initialization.js
+++ initialization/initialization.js
@@ -13,7 +13,7 @@ const { createNotFoundError } = require('~/utils/errorsHelper')
 const errorMiddleware = require('~/middlewares/error')
 
 const initialization = (app) => {
-  app.use(express.json())
+  app.use(express.json({ limit: '10mb' }))
   app.use(express.urlencoded({ extended: true }))
   app.use(cookieParser())
   app.use(

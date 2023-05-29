f4bcc006a843e19b7ca3d69b8684890d59363dcd
mxrcury <demetrgonchar@gmail.com>
2023-05-29 18:40:04 +0300

    refactor

--- utils/cooperations/coopsAggregateOptions.js
+++ utils/cooperations/coopsAggregateOptions.js
@@ -1,6 +1,7 @@
 const getRegex = require('../getRegex')
 
 const coopsAggregateOptions = (params = {}, query) => {
+  const { id } = params
   const { skip = 0, limit = 5, status = '', order = 'updatedAt', orderBy = 'asc', search } = query
 
   const match = {}
@@ -9,9 +10,7 @@ const coopsAggregateOptions = (params = {}, query) => {
   if (order) sort[order] = orderBy
 
   if (status) match.status = getRegex(status)
-  if (id) {
-    match['$or'] = [{ initiatorUserId: id }, { recipientUserId: id }]
-  }
+  if (id) match.$or = [{ initiator: id }, { receiver: id }]
 
   if (search) match.fullName = getRegex(search)
 

0d7155ded1f6107d6de99536526ba95bd9d54011
Pavlo Diakun <rubber883@gmail.com>
2023-07-05 19:36:23 +0300

    delete console.log

--- controllers/chat.js
+++ controllers/chat.js
@@ -3,7 +3,7 @@ const chatService = require('~/services/chat')
 const createChat = async (req, res) => {
   const { id: author, role: authorRole } = req.user
   const { chatMember, chatMemberRole } = req.body
-  console.log(req.body)
+
   const chat = await chatService.createChat({
     members: [
       { user: author, role: authorRole },

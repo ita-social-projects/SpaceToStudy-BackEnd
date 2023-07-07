8281ef90274c87d8f7953719ba8d68631a853263
mxrcury <demetrgonchar@gmail.com>
2023-07-07 09:28:08 +0300

    added test and docs

--- routes/notification.js
+++ routes/notification.js
@@ -2,6 +2,9 @@ const router = require('express').Router({ mergeParams: true })
 
 const notificationController = require('~/controllers/notification')
 const asyncWrapper = require('~/middlewares/asyncWrapper')
+const { authMiddleware } = require('~/middlewares/auth')
+
+router.use(authMiddleware)
 
 router.get('/', asyncWrapper(notificationController.getNotifications))
 
--- services/notification.js
+++ services/notification.js
@@ -2,7 +2,7 @@ const Notification = require('~/models/notification')
 
 const notificationService = {
   getNotifications: async (match) => {
-    const notifications = await Notification.find(match).countDocuments().exec()
+    const notifications = await Notification.find(match).exec()
     const count = await Notification.countDocuments(match)
 
     return {

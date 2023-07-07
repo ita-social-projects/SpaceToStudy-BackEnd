d9c1132deb189cb5b055b72216191d2e940809c0
mxrcury <demetrgonchar@gmail.com>
2023-07-07 10:21:20 +0300

    modified tests

--- controllers/course.js
+++ controllers/course.js
@@ -1,14 +1,14 @@
 const courseService = require('~/services/course')
 
-const updateCourse = async (req,res) => {
+const updateCourse = async (req, res) => {
   const { id: userId } = req.user
   const { id } = req.params
   const data = req.body
 
-  await courseService.updateCourse(userId, {...data, id})
+  await courseService.updateCourse(userId, { ...data, id })
 
   res.status(204).end()
-}  
+}
 
 module.exports = {
   updateCourse
--- test/integration/controllers/course.spec.js
+++ test/integration/controllers/course.spec.js
@@ -4,35 +4,38 @@ const testUserAuthentication = require('~/utils/testUserAuth')
 const Course = require('~/models/course')
 const { expectError } = require('~/test/helpers')
 const { UNAUTHORIZED, DOCUMENT_NOT_FOUND, FORBIDDEN } = require('~/consts/errors')
-
+const TokenService = require('~/services/token')
 const endpointUrl = '/courses/'
 
 const nonExistingCourseId = '64a51e41de4debbccf0b39b0'
 
 let tutorUser = {
-  role: ['tutor'],
+  role: 'tutor',
   firstName: 'albus',
   lastName: 'dumbledore',
   email: 'lovemagic@gmail.com',
   password: 'supermagicpass123',
   appLanguage: 'en',
+  FAQ: { student: [{ question: 'question1', answer: 'answer1' }] },
   isEmailConfirmed: true,
-  lastLogin: new Date().toJSON()
+  lastLogin: new Date().toJSON(),
+  lastLoginAs: 'tutor'
 }
 
 const testCourseData = {
   title: 'assembly',
   description: 'you will learn some modern programming language for all your needs',
-  attachments: ['http://link.com/file1']
+  attachments: ['http://link.com/file1'],
+  lessons: []
 }
 
-const updateData = { 
-  title:'new title',
-  description:'new description'
+const updateData = {
+  title: 'new title',
+  description: 'new description'
 }
 
 describe('Course controller', () => {
-  let app, server, accessToken, studentAccessToken, testCourse
+  let app, server, accessToken, currentUser, studentAccessToken, testCourse
 
   beforeAll(async () => {
     ;({ app, server } = await serverInit())
@@ -42,10 +45,14 @@ describe('Course controller', () => {
     await checkCategoryExistence()
 
     accessToken = await testUserAuthentication(app, tutorUser)
-
     studentAccessToken = await testUserAuthentication(app)
-
-    testCourse = await app.post(endpointUrl).set('Authorization', `Bearer ${accessToken}`).send(testCourseData)
+    
+    currentUser = TokenService.validateAccessToken(accessToken)
+    
+    testCourse = await Course.create({
+      ...testCourseData,
+      author: currentUser.id
+    })
   })
 
   afterEach(async () => {
@@ -57,8 +64,11 @@ describe('Course controller', () => {
   })
 
   describe(`PATCH ${endpointUrl}:id`, () => {
-    it('should updated a course', async () => {
-      const response = await app.patch(endpointUrl + testCourse._id).set('Authorization', `Bearer ${accessToken}`).send(updateData)
+    it('should update a course', async () => {
+      const response = await app
+        .patch(endpointUrl + testCourse._id)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(updateData)
 
       const { title, description } = await Course.findById(testCourse._id)
 
@@ -68,19 +78,22 @@ describe('Course controller', () => {
 
     it('should throw UNAUTHORIZED', async () => {
       const response = await app.patch(endpointUrl)
-  
+
       expectError(401, UNAUTHORIZED, response)
     })
 
     it('should throw DOCUMENT_NOT_FOUND', async () => {
       const response = await app.patch(endpointUrl + nonExistingCourseId).set('Authorization', `Bearer ${accessToken}`)
-  
+
       expectError(404, DOCUMENT_NOT_FOUND([Course.modelName]), response)
     })
 
     it('should throw FORBIDDEN', async () => {
-      const response = await app.patch(endpointUrl).set('Authorization', `Bearer ${studentAccessToken}`).send(updateData)
-  
+      const response = await app
+        .patch(endpointUrl)
+        .set('Authorization', `Bearer ${studentAccessToken}`)
+        .send(updateData)
+
       expectError(403, FORBIDDEN, response)
     })
   })

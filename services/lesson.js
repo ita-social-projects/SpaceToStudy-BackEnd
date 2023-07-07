05a5963b7bea402118c3e86b6aebb1c16dc76cd8
Diana Tsivka <tsivkadi@gmail.com>
2023-07-07 09:39:59 +0300

    bag fix

--- services/lesson.js
+++ services/lesson.js
@@ -1,12 +1,14 @@
 const Lesson = require('~/models/lesson')
 const uploadService = require('~/services/upload')
-const { LESSON } = require('~/consts/upload')
+const { ATTACHMENT } = require('~/consts/upload')
 
 const lessonService = {
   createLesson: async (author, data) => {
     let { title, description, attachments } = data
 
-    const fileUrls = await Promise.all(attachments.map(async (file) => await uploadService.uploadFile(file, LESSON)))
+    const fileUrls = await Promise.all(
+      attachments.map(async (file) => await uploadService.uploadFile(file, ATTACHMENT))
+    )
 
     return await Lesson.create({ author, title, description, attachments: fileUrls })
   }

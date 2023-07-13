37d79e58b64e2bdfe21b8d24c8a92392c34271fc
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2023-07-13 22:08:30 +0300

    route moved

--- routes/course.js
+++ routes/course.js
@@ -18,10 +18,10 @@ router.use(authMiddleware)
 
 router.param('id', idValidation)
 
+router.get('/:id', isEntityValid({ params }), asyncWrapper(courseController.getCourseById))
 router.use(restrictTo(TUTOR))
 router.get('/', asyncWrapper(courseController.getCourses))
 router.post('/', isEntityValid({ body }), asyncWrapper(courseController.createCourse))
-router.patch('/:id', isEntityValid({ params }),asyncWrapper(courseController.updateCourse))
-router.get('/:id', isEntityValid({ params }), asyncWrapper(courseController.getCourseById))
+router.patch('/:id', isEntityValid({ params }), asyncWrapper(courseController.updateCourse))
 
 module.exports = router

53bd3dad091063ed35875ed260627c17e2aa6b59
Yura Didenko <didenkoyura225@gmail.com>
2023-07-24 11:29:47 +0300

    Added max and min length

--- models/attachments.js
+++ models/attachments.js
@@ -1,19 +1,23 @@
 const { Schema, model } = require('mongoose')
-const { FIELD_CANNOT_BE_EMPTY } = require('~/consts/errors')
+const { FIELD_CANNOT_BE_EMPTY, FIELD_CANNOT_BE_SHORTER, FIELD_CANNOT_BE_LONGER } = require('~/consts/errors')
 const { ATTACHMENT } = require('~/consts/models')
 
 const attachmentSchema = new Schema(
   {
     fileName: {
       type: String,
-      required: [true, FIELD_CANNOT_BE_EMPTY('fileName')]
+      required: [true, FIELD_CANNOT_BE_EMPTY('fileName')],
+      minLength: [5, FIELD_CANNOT_BE_SHORTER('file name', 5)],
+      maxLength: [100, FIELD_CANNOT_BE_LONGER('file name', 100)]
     },
     link: {
       type: String,
       required: [true, FIELD_CANNOT_BE_EMPTY('link')]
     },
     description: {
-      type: String
+      type: String,
+      minLength: [30, FIELD_CANNOT_BE_SHORTER('description', 30)],
+      maxLength: [1000, FIELD_CANNOT_BE_LONGER('description', 1000)]
     },
     contentType: {
       type: String,

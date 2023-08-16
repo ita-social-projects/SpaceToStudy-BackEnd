bdc06b8b0bafabbbb422129e2c705b3eed0191fb
Tetiana Deresh <tetiana.deresh.knm.2020@lpnu.ua>
2023-08-16 22:06:30 +0300

    Created finishedLesson model

---
+++ models/finishedLesson.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose')
+const { LESSON, FINISHED_LESSON } = require('~/consts/models')
+const { FIELD_CANNOT_BE_EMPTY, ENUM_CAN_BE_ONE_OF } = require('~/consts/errors')
+
+const finishedLessonSchema = new Schema(
+  {
+    lesson: {
+      type: Schema.Types.ObjectId,
+      ref: LESSON,
+      required: [true, FIELD_CANNOT_BE_EMPTY('lesson')]
+    },
+    status: {
+      type: String,
+      enum: {
+        values: LESSON_STATUS_ENUM,
+        message: ENUM_CAN_BE_ONE_OF('lesson status', LESSON_STATUS_ENUM)
+      },
+      default: LESSON_STATUS_ENUM[0]
+    }
+  },
+
+  {
+    timestamps: true,
+    versionKey: false
+  }
+)
+
+module.exports = model(FINISHED_LESSON, finishedLessonSchema)

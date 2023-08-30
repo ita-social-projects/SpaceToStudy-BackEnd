01b128f4f1e2a5ac43aa82c317c88237c4b53b42
Yura Didenko <didenkoyura225@gmail.com>
2023-08-30 12:28:11 +0300

    Fixed codesmell

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,9 +1,11 @@
 const multer = require('multer')
 
+const storage = multer.memoryStorage()
 const upload = multer({
-  storage: multer.memoryStorage(),
+  storage,
   limits: {
-    fileSize: 10 * 1024 * 1024
+    fileSize: 10 * 1024 * 1024,
+    fieldNameSize: 100
   }
 })
 

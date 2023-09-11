00bb11c33559682181b4da4f9792d2c0c7d7eb9d
mxrcury <demetrgonchar@gmail.com>
2023-09-11 15:52:52 +0300

    some fixes

--- services/question.js
+++ services/question.js
@@ -2,8 +2,9 @@ const Question = require('~/models/question')
 
 const questionService = {
   getQuestions: async (match, sort, skip = 0, limit = 10) => {
-    const items = await Question.collation({ locale: 'en', strength: 1 })
+    const items = await Question
       .find(match)
+      .collation({ locale: 'en', strength: 1 })
       .sort(sort)
       .skip(skip)
       .limit(limit)

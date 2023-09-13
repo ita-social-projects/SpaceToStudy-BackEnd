c740b97a45461a11a6a82cf4c6aad737933099d2
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2023-09-13 20:21:11 +0300

    fixed naming

--- models/resourcesCategory.js
+++ models/resourcesCategory.js
@@ -7,9 +7,9 @@ const resourcesCategorySchema = new Schema(
   {
     name: {
       type: String,
-      required: [true, FIELD_CANNOT_BE_EMPTY('title')],
-      minLength: [1, FIELD_CANNOT_BE_SHORTER('title', 1)],
-      maxLength: [50, FIELD_CANNOT_BE_LONGER('title', 50)]
+      required: [true, FIELD_CANNOT_BE_EMPTY('name')],
+      minLength: [1, FIELD_CANNOT_BE_SHORTER('name', 1)],
+      maxLength: [50, FIELD_CANNOT_BE_LONGER('name', 50)]
     },
     author: {
       type: Schema.Types.ObjectId,

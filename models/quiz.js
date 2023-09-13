b0bce1994ffe35b8e3cb5c9dfd7f5f3fb7f96a61
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2023-09-13 20:00:53 +0300

    modify quiz model

--- models/quiz.js
+++ models/quiz.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose')
-const { QUIZ, USER } = require('~/consts/models')
+const { QUIZ, USER, QUESTION } = require('~/consts/models')
 const { FIELD_CANNOT_BE_EMPTY, FIELD_CANNOT_BE_LONGER, FIELD_CANNOT_BE_SHORTER } = require('~/consts/errors')
 
 const quizSchema = new Schema(
@@ -10,33 +10,11 @@ const quizSchema = new Schema(
       minLength: [1, FIELD_CANNOT_BE_SHORTER('title', 1)],
       maxLength: [100, FIELD_CANNOT_BE_LONGER('title', 100)]
     },
-    items: [
-      {
-        _id: false,
-        question: {
-          type: String,
-          required: [true, FIELD_CANNOT_BE_EMPTY('question')],
-          minLength: [1, FIELD_CANNOT_BE_SHORTER('question', 1)],
-          maxLength: [150, FIELD_CANNOT_BE_LONGER('question', 150)]
-        },
-        answers: [
-          {
-            _id: false,
-            text: {
-              type: String,
-              required: [true, FIELD_CANNOT_BE_EMPTY('answer')],
-              minLength: [1, FIELD_CANNOT_BE_SHORTER('answer', 1)],
-              maxLength: [150, FIELD_CANNOT_BE_LONGER('answer', 150)]
-            },
-            isCorrect: {
-              type: Boolean,
-              default: false,
-              required: true
-            }
-          }
-        ]
-      }
-    ],
+    items: {
+      type: [Schema.Types.ObjectId],
+      ref: QUESTION,
+      required: true
+    },
     author: {
       type: Schema.Types.ObjectId,
       ref: USER,

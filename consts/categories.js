6b02f3883c2da8c33f7b5a5863a22c72fc57fa60
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2023-12-12 21:00:22 +0200

    set different colors for categories

--- consts/categories.js
+++ consts/categories.js
@@ -7,52 +7,52 @@ const categories = {
   MATHEMATICS: {
     _id: '64884f33fdc2d1a130c24ac2',
     name: 'Mathematics',
-    appearance: { path: 'mocked-path-to-icon', color: '#66C42C' }
+    appearance: { path: 'mocked-path-to-icon', color: '#866606' }
   },
   COMPUTER_SCIENCE: {
     _id: '64884fedfdc2d1a130c24ade',
     name: 'Computer science',
-    appearance: { path: 'mocked-path-to-icon', color: '#66C42C' }
+    appearance: { path: 'mocked-path-to-icon', color: '#462027' }
   },
   MUSIC: {
     _id: '64884f4dfdc2d1a130c24ac6',
     name: 'Music',
-    appearance: { path: 'mocked-path-to-icon', color: '#66C42C' }
+    appearance: { path: 'mocked-path-to-icon', color: '#F67C41' }
   },
   DESIGN: {
     _id: '64884f59fdc2d1a130c24ac8',
     name: 'Design',
-    appearance: { path: 'mocked-path-to-icon', color: '#66C42C' }
+    appearance: { path: 'mocked-path-to-icon', color: '#866606' }
   },
   HISTORY: {
     _id: '64884f66fdc2d1a130c24aca',
     name: 'History',
-    appearance: { path: 'mocked-path-to-icon', color: '#66C42C' }
+    appearance: { path: 'mocked-path-to-icon', color: '#462027' }
   },
   BIOLOGY: {
     _id: '64884f70fdc2d1a130c24acc',
     name: 'Biology',
-    appearance: { path: 'mocked-path-to-icon', color: '#66C42C' }
+    appearance: { path: 'mocked-path-to-icon', color: '#F67C41' }
   },
   PAINTING: {
     _id: '64884f7afdc2d1a130c24ace',
     name: 'Painting',
-    appearance: { path: 'mocked-path-to-icon', color: '#66C42C' }
+    appearance: { path: 'mocked-path-to-icon', color: '#5E0813' }
   },
   FINANCES: {
     _id: '64884f83fdc2d1a130c24ad0',
     name: 'Finances',
-    appearance: { path: 'mocked-path-to-icon', color: '#66C42C' }
+    appearance: { path: 'mocked-path-to-icon', color: '#D0C59B' }
   },
   MARKETING: {
     _id: '64884f8efdc2d1a130c24ad2',
     name: 'Marketing',
-    appearance: { path: 'mocked-path-to-icon', color: '#66C42C' }
+    appearance: { path: 'mocked-path-to-icon', color: '#FFAAA5' }
   },
   AUDIT: {
     _id: '64884f98fdc2d1a130c24ad4',
     name: 'Audit',
-    appearance: { path: 'mocked-path-to-icon', color: '#66C42C' }
+    appearance: { path: 'mocked-path-to-icon', color: '#FFD700' }
   },
   CHEMISTRY: {
     _id: '64884fa2fdc2d1a130c24ad6',
@@ -62,17 +62,17 @@ const categories = {
   ASTRONOMY: {
     _id: '64884fb0fdc2d1a130c24ad8',
     name: 'Astronomy',
-    appearance: { path: 'mocked-path-to-icon', color: '#66C42C' }
+    appearance: { path: 'mocked-path-to-icon', color: '#866606' }
   },
   PHYSICS: {
     _id: '64884fc3fdc2d1a130c24ada',
     name: 'Physics',
-    appearance: { path: 'mocked-path-to-icon', color: '#66C42C' }
+    appearance: { path: 'mocked-path-to-icon', color: '#462027' }
   },
   PSYCHOLOGY: {
     _id: '64884fe3fdc2d1a130c24adc',
     name: 'Psychology',
-    appearance: { path: 'mocked-path-to-icon', color: '#66C42C' }
+    appearance: { path: 'mocked-path-to-icon', color: '#D0C59B' }
   }
 }
 

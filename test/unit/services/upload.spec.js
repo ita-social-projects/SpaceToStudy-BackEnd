5e63d2241a806778560958adfcd92e918e782045
Александр Мигаль <magalalecs2003@gmail.com>
2024-02-01 12:57:32 +0200

    fix coverage at upload

--- test/unit/services/upload.spec.js
+++ test/unit/services/upload.spec.js
@@ -79,4 +79,74 @@ describe('uploadService`', () => {
         expect(err).toBe('error')
       }
     })
+  it('should update a file in Azure Blob Storage successfully', async () => {
+    const file = {
+      buffer: 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAgAAAQABAAD...',
+      name: 'example.jpg',
+      newName: 'exampleName.jpg'
+    }
+    const blobNameNew = `${file.newName}`
+
+    const fn = (blobUrl, containerName, newBlobName, cb) => {
+      cb(null, newBlobName)
+    }
+    const fnP = (container, blobName, cb) => {
+      cb(null, { copy: { status: 'success' } })
+    }
+
+    const blobServiceStub = {
+      startCopyBlob: fn,
+      getBlobProperties: fnP
+    }
+    azureStorage.createBlobService.mockImplementationOnce(() => blobServiceStub)
+
+    const result = await uploadService.updateFile(file.name, file.newName, file.buffer)
+
+    expect(result).toContain(blobNameNew)
+  })
+  it('Should show an err on startCopyBlob during the update', async () => {
+    const file = {
+      buffer: 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAgAAAQABAAD...',
+      name: 'example.jpg',
+      newName: 'exampleName.jpg'
+    }
+    const fn = (blobUrl, containerName, newBlobName, cb) => {
+      cb('error', newBlobName)
+    }
+    const blobServiceStub = {
+      startCopyBlob: fn
+    }
+    azureStorage.createBlobService.mockImplementationOnce(() => blobServiceStub)
+
+    try {
+      await uploadService.updateFile(file.name, file.newName, file.buffer)
+    } catch (err) {
+      expect(err).toBe('error')
+    }
+  })
+
+  it('Should show an err on getBlobProperties during the update', async () => {
+    const file = {
+      buffer: 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAgAAAQABAAD...',
+      name: 'example.jpg',
+      newName: 'exampleName.jpg'
+    }
+    const fn = (blobUrl, containerName, newBlobName, cb) => {
+      cb(null, newBlobName)
+    }
+    const fnP = (container, blobName, cb) => {
+      cb('error', blobName)
+    }
+    const blobServiceStub = {
+      startCopyBlob: fn,
+      getBlobProperties: fnP
+    }
+    azureStorage.createBlobService.mockImplementationOnce(() => blobServiceStub)
+
+    try {
+      await uploadService.updateFile(file.name, file.newName, file.buffer)
+    } catch (err) {
+      expect(err).toBe('error')
+    }
+  })
 })

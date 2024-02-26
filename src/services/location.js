ec532e3a9b645afec8a55b58585a165bf6751295
dmtrth25 <vodolazhchenko25@gmail.com>
2024-02-26 17:21:18 +0200

    fix for stepper countries

--- src/services/location.js
+++ src/services/location.js
@@ -1,23 +1,31 @@
 const { request } = require('gaxios')
 
-const API_URL = 'https://countriesnow.space/api/v0.1'
+const API_URL = 'https://api.countrystatecity.in/v1/countries'
+const API_KEY = 'bW94V3phZU84YkhERVNUQmFOOFRDTVpIU3BFWFFJR1BsQUNZRXhjdg=='
 
 const locationService = {
   getCountries: async () => {
-    const res = await request({ url: `${API_URL}/countries/states` })
-    const countries = res.data.data.map((country) => country.name)
+    const res = await request({
+      url: API_URL,
+      headers: {
+        'X-CSCAPI-KEY': API_KEY
+      }
+    })
+    const countries = res.data.map(({ name, iso2 }) => ({ name, iso2 }))
 
     return [...new Set(countries)]
   },
 
-  getCities: async (country) => {
+  getCities: async (countryCode) => {
     const res = await request({
-      method: 'POST',
-      url: `${API_URL}/countries/cities`,
-      data: { country }
+      url: `${API_URL}/${countryCode}/cities`,
+      headers: {
+        'X-CSCAPI-KEY': API_KEY
+      }
     })
+    const cities = res.data.map((city) => city.name)
 
-    return res.data.data
+    return [...new Set(cities)]
   }
 }
 

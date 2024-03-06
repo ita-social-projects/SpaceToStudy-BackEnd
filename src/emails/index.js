d37a7002bd025631dae5a88503f3268eeaae6b8b
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2024-03-06 12:31:39 +0200

    fixed tests

--- src/emails/index.js
+++ src/emails/index.js
@@ -1,54 +1,55 @@
 const emailSubject = require('~/consts/emailSubject')
 
+const path = __dirname
 const templateList = {
   [emailSubject.EMAIL_CONFIRMATION]: {
     en: {
       subject: 'Please confirm your email',
-      template: 'en/confirm-email'
+      template: path + '/en/confirm-email'
     },
     ua: {
       subject: 'Будь ласка, підтвердіть свою електронну адресу',
-      template: 'ua/confirm-email'
+      template: path + '/ua/confirm-email'
     }
   },
   [emailSubject.RESET_PASSWORD]: {
     en: {
       subject: 'Reset your account password',
-      template: 'en/reset-password'
+      template: path + '/en/reset-password'
     },
     ua: {
       subject: 'Скиньте пароль для свого акаунту',
-      template: 'ua/reset-password'
+      template: path + '/ua/reset-password'
     }
   },
   [emailSubject.SUCCESSFUL_PASSWORD_RESET]: {
     en: {
       subject: 'Your password was changed',
-      template: 'en/sucessful-password-reset'
+      template: path + '/en/sucessful-password-reset'
     },
     ua: {
       subject: 'Ваш пароль було змінено',
-      template: 'ua/sucessful-password-reset'
+      template: path + '/ua/sucessful-password-reset'
     }
   },
   [emailSubject.LONG_TIME_WITHOUT_LOGIN]: {
     en: {
       subject: 'You have been inactive for too long',
-      template: 'en/long-time-without-login'
+      template: path + '/en/long-time-without-login'
     },
     ua: {
       subject: 'Ви занадто довго були неактивні',
-      template: 'ua/long-time-without-login'
+      template: path + '/ua/long-time-without-login'
     }
   },
   [emailSubject.ADMIN_INVITATION]: {
     en: {
       subject: 'Admin invitation',
-      template: 'en/invite-admin'
+      template: path + '/en/invite-admin'
     },
     ua: {
       subject: 'Запрошення адміна',
-      template: 'ua/invite-admin'
+      template: path + '/ua/invite-admin'
     }
   }
 }

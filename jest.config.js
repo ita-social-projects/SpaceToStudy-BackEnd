fc26986c55858c4d3e551f527cf8f354aa1d0696
Olena Hryk <olena.andrushchakevych@gmail.com>
2024-07-03 14:03:02 +0300

    Test: increase jest.setTimeout(30000)

--- jest.config.js
+++ jest.config.js
@@ -26,10 +26,7 @@ module.exports = {
   },
   coverageReporters: ['html', 'lcov'],
   coverageDirectory: '<rootDir>/src/test/coverage',
-  testTimeout: 12000,
-  testMatch: [
-    '<rootDir>/src/test/integration/**/*.spec.js',
-    '<rootDir>/src/test/unit/**/*.spec.js'
-  ],
+  testTimeout: 30000,
+  testMatch: ['<rootDir>/src/test/integration/**/*.spec.js', '<rootDir>/src/test/unit/**/*.spec.js'],
   testResultsProcessor: 'jest-sonar-reporter'
 }

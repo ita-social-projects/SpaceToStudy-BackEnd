045fcf17dafa55a6d56c8d6e4be8e4edc5816cab
PavloDolia <paveldolia93@gmail.com>
2024-08-13 10:49:01 +0300

    fixed user router

--- src/routes/user.js
+++ src/routes/user.js
@@ -30,11 +30,6 @@ router.get('/:id', isEntityValid({ params }), asyncWrapper(userController.getUse
 router.patch('/:id', isEntityValid({ params }), asyncWrapper(userController.updateUser))
 router.patch('/deactivate/:id', isEntityValid({ params }), asyncWrapper(userController.deactivateUser))
 router.patch('/activate/:id', isEntityValid({ params }), asyncWrapper(userController.activateUser))
-router.patch(
-  '/:id/bookmarks/offers/:offerId',
-  isEntityValid({ params }),
-  asyncWrapper(userController.toggleOfferBookmark)
-)
 
 router.use(restrictTo(ADMIN))
 router.patch('/:id/change-status', isEntityValid({ params }), asyncWrapper(userController.updateStatus))

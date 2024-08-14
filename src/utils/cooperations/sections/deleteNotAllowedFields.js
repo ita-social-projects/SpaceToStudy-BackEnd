1cad878b93a1fa62b0d96a8a9074779210a79a7a
YaroslavChuiko <jarik.tchuicko@gmail.com>
2024-08-14 09:28:16 +0300

    deleted coopSectionsValidation middleware

--- src/utils/cooperations/sections/deleteNotAllowedFields.js
+++
@@ -1,9 +0,0 @@
-const deleteNotAllowedFields = (resource, allowedFields) => {
-  Object.keys(resource).forEach((property) => {
-    if (!allowedFields.includes(property)) {
-      delete resource[property]
-    }
-  })
-}
-
-module.exports = deleteNotAllowedFields

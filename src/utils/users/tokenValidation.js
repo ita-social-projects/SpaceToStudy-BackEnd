6e82184db31057bfcb750a5cef31b79c121a4a05
docia <matiushenko107@gmail.com>
2024-09-12 22:01:03 +0300

    update tokenValidation.js

--- src/utils/users/tokenValidation.js
+++ src/utils/users/tokenValidation.js
@@ -1,20 +1,19 @@
 const tokenService = require('~/services/token')
-const ValidationError = require('~/consts/errors')
+const { UNAUTHORIZED } = require('~/consts/errors')
+const { FORBIDDEN } = require('~/consts/errors')
 
 const validateUserToken = (req, res) => {
   const accessToken = req?.cookies?.accessToken
 
-  if (!accessToken) return res.status(401).end()
+  if (!accessToken) return res.status(401).json(UNAUTHORIZED)
 
   const decodedToken = tokenService.validateAccessToken(accessToken)
 
-  if (!decodedToken) return res.status(401).end()
+  if (!decodedToken) return res.status(401).json(UNAUTHORIZED)
 
-  if (decodedToken.id !== req?.params?.userId) {
-    return ValidationError
-  }
+  if (decodedToken.id !== req?.params?.id) return res.status(403).json(FORBIDDEN)
 
-  return decodedToken
+  return true
 }
 
 module.exports = validateUserToken

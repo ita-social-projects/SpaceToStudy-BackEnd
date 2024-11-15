a29c036b15436efaa86a205685e0cad4960141e4
luiqor <vohnenna@gmail.com>
2024-11-15 18:39:18 +0200

    refactor entity validation by adding closure function

--- src/middlewares/entityValidation.js
+++ src/middlewares/entityValidation.js
@@ -1,55 +1,62 @@
 const { DOCUMENT_NOT_FOUND } = require('~/consts/errors')
 const { createError } = require('~/utils/errorsHelper')
 
-const validateSingleEntity = async (id, model, models) => {
-  if (!id) {
+const createEntityValidator = (invalidModels) => {
+  return async (id, model) => {
+    if (!id) {
+      return
+    }
+
+    const document = await model.findById(id)
+
+    if (!document && !invalidModels.includes(model.modelName)) {
+      invalidModels.push(model.modelName)
+    }
+  }
+}
+
+const validateEntitiesFromParams = async (entitiesParams, reqParams, validateSingleEntity) => {
+  if (!entitiesParams?.length) {
     return
   }
 
-  const document = await model.findById(id)
+  for (const { model, idName } of entitiesParams) {
+    const id = reqParams[idName]
 
-  if (!document && !models.includes(model.modelName)) {
-    models.push(model.modelName)
+    await validateSingleEntity(id, model)
   }
 }
 
-const validateEntitiesFromParams = async (params, req, models) => {
-  if (params?.length) {
-    for (const { model, idName } of params) {
-      const id = req.params[idName]
-
-      await validateSingleEntity(id, model, models)
-    }
+const validateEntitiesFromBody = async (entitiesBody, reqBody, validateSingleEntity) => {
+  if (!entitiesBody?.length) {
+    return
   }
-}
 
-const validateEntitiesFromBody = async (body, req, models) => {
-  if (body?.length) {
-    for (const { model, idName } of body) {
-      if (Array.isArray(req.body[idName])) {
-        await Promise.all(
-          req.body[idName].map(async (id) => {
-            await validateSingleEntity(id, model, models)
-          })
-        )
-      } else {
-        const id = req.body[idName]
-        await validateSingleEntity(id, model, models)
-      }
+  for (const { model, idName } of entitiesBody) {
+    if (Array.isArray(reqBody[idName])) {
+      await Promise.all(
+        reqBody[idName].map(async (id) => {
+          await validateSingleEntity(id, model)
+        })
+      )
+      return
     }
+
+    const id = reqBody[idName]
+    await validateSingleEntity(id, model)
   }
 }
 
 const isEntityValid = (entities) => {
   return async (req, _res, next) => {
-    const models = []
+    const invalidModels = []
 
-    await validateEntitiesFromParams(entities.params, req, models)
+    await validateEntitiesFromParams(entities.params, req.params, createEntityValidator(invalidModels))
 
-    await validateEntitiesFromBody(entities.body, req, models)
+    await validateEntitiesFromBody(entities.body, req.body, createEntityValidator(invalidModels))
 
-    if (models.length) {
-      next(createError(404, DOCUMENT_NOT_FOUND(models)))
+    if (invalidModels.length) {
+      next(createError(404, DOCUMENT_NOT_FOUND(invalidModels)))
     }
 
     next()

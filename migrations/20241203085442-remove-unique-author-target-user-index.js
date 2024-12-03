607542de5cba6cd841f9b325771390bdae0c7baf
luiqor <vohnenna@gmail.com>
2024-12-03 11:06:27 +0200

    add author_1_targetUserId_1 index migration

--- migrations/20241203085442-remove-unique-author-target-user-index.js
+++ migrations/20241203085442-remove-unique-author-target-user-index.js
@@ -5,11 +5,9 @@ const indexName = 'author_1_targetUserId_1'
 module.exports = {
   async up(db) {
     await db.collection(collectionName).dropIndex(indexName)
-    await db.collection(collectionName).createIndex({ author: 1, targetUserId: 1 })
   },
 
   async down(db) {
-    await db.collection(collectionName).dropIndex(indexName)
     await db.collection(collectionName).createIndex({ author: 1, targetUserId: 1 }, { unique: true })
   }
 }

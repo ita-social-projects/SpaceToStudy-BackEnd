b77ffe79e8645c7708a23099824cfb43339b1daf
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2024-12-04 03:38:34 +0200

    add DEFAULT_AGGREGATION_UNSELECTABLE_USER_FIELDS constant

---
+++ src/consts/user.js
@@ -0,0 +1,12 @@
+const DEFAULT_AGGREGATION_UNSELECTABLE_USER_FIELDS = {
+  appLanguage: false,
+  bookmarkedOffers: false,
+  isEmailConfirmed: false,
+  isFirstLogin: false,
+  lastLoginAs: false,
+  password: false
+}
+
+module.exports = {
+  DEFAULT_AGGREGATION_UNSELECTABLE_USER_FIELDS
+}

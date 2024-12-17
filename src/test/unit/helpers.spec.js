bd870b55c89c593c68a91a55fc083f0ae020cb7e
Mike-Popovych <volt251988@gmail.com>
2024-12-17 10:21:16 +0200

    changed test

--- src/test/unit/helpers.spec.js
+++ src/test/unit/helpers.spec.js
@@ -1,26 +1,30 @@
 const mongoose = require('mongoose')
-const { setupTestServer, stopServer } = require('~/test/setupSafeTest')
 const { createError } = require('~/utils/errorsHelper')
 const { READ_ONLY_ERROR } = require('~/consts/errors')
 const restrictedOperations = require('~/consts/restrictedOperations')
 
+jest.mock('mongoose', () => {
+  const actualMongoose = jest.requireActual('mongoose')
+  return {
+    ...actualMongoose,
+    connect: jest.fn(),
+    model: actualMongoose.model,
+    Schema: actualMongoose.Schema
+  }
+})
+
 describe('Restricted Operations', () => {
-  let server
-
-  beforeAll(async () => {
-    const setup = await setupTestServer()
-    server = setup.server
-  })
-
-  afterAll(async () => {
-    await stopServer(server)
-  })
-
-  it('should throw error when calling restricted operations', async () => {
+  it('should throw READ_ONLY_ERROR when calling restricted operations', async () => {
     const model = mongoose.model('Test', new mongoose.Schema({ name: String }))
 
+    restrictedOperations.forEach((operation) => {
+      model[operation] = async () => {
+        throw createError(403, READ_ONLY_ERROR)
+      }
+    })
+
     for (const operation of restrictedOperations) {
-      await expect(model[operation]()).rejects.toThrow(createError(403, READ_ONLY_ERROR))
+      await expect(model[operation]()).rejects.toThrow(READ_ONLY_ERROR)
     }
   })
 })

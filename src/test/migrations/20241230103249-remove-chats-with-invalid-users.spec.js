5ef835fb18e0704b85e89d4dacefb60a1689695c
Mike-Popovych <volt251988@gmail.com>
2024-12-31 12:16:31 +0200

    added chat migration test

---
+++ src/test/migrations/20241230103249-remove-chats-with-invalid-users.spec.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose')
+const Chats = require('~/models/chat')
+const Users = require('~/models/user')
+const { serverInit, serverCleanup, stopServer } = require('~/test/setup')
+const migration = require('@root/migrations/20241230103249-remove-chats-with-invalid-users')
+
+describe('Migration - Remove chats with invalid members', () => {
+  let server
+
+  beforeAll(async () => {
+    ;({ server } = await serverInit())
+  })
+
+  afterEach(async () => {
+    await serverCleanup()
+  })
+
+  afterAll(async () => {
+    await stopServer(server)
+  })
+
+  const insertUsers = async (data) => {
+    await Users.insertMany(data)
+  }
+
+  const insertChats = async (data) => {
+    await Chats.insertMany(data)
+  }
+
+  const getChats = async () => {
+    return Chats.find({}).lean()
+  }
+
+  it('Should remove chats with invalid members', async () => {
+    const validUser = new mongoose.Types.ObjectId()
+    const invalidUserId = new mongoose.Types.ObjectId()
+
+    await insertUsers([
+      {
+        _id: validUser,
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john.doe@example.com',
+        password: 'securepassword123'
+      }
+    ])
+
+    const validChat = {
+      _id: new mongoose.Types.ObjectId(),
+      members: [{ user: validUser, role: 'student' }]
+    }
+
+    const invalidChat = {
+      _id: new mongoose.Types.ObjectId(),
+      members: [{ user: invalidUserId, role: 'tutor' }]
+    }
+
+    await insertChats([validChat, invalidChat])
+
+    await migration.up(mongoose.connection.db)
+
+    const remainingChats = await getChats()
+    expect(remainingChats).toHaveLength(1)
+    expect(remainingChats[0]._id.toString()).toBe(validChat._id.toString())
+  })
+
+  it('Should do nothing in the down migration', async () => {
+    await expect(migration.down(mongoose.connection.db)).resolves.not.toThrow()
+  })
+})

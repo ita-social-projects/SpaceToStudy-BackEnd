e91f62106a62856edea8c2c1ea52aab29dd471d1
luiqor <vohnenna@gmail.com>
2025-01-09 11:30:52 +0200

    add all fields required test

--- src/test/integration/models/subject.spec.js
+++ src/test/integration/models/subject.spec.js
@@ -1,3 +1,4 @@
+const Subject = require('~/models/subject')
 const { setupTestServer, stopServer } = require('~/test/setupSafeTest')
 
 describe('Subject model', () => {
@@ -10,5 +11,19 @@ describe('Subject model', () => {
 
   afterAll(async () => {
     await stopServer(server)
+  })
+
+  it('should include all required fields in the subject documents', async () => {
+    const subjects = await Subject.find({}).limit(100)
+
+    for (const subject of subjects) {
+      expect(subject).toHaveProperty('name')
+      expect(subject).toHaveProperty('category')
+      expect(subject).toHaveProperty('totalOffers')
+      expect(subject.totalOffers).toHaveProperty('student')
+      expect(subject.totalOffers).toHaveProperty('tutor')
+      expect(subject).toHaveProperty('createdAt')
+      expect(subject).toHaveProperty('updatedAt')
+    }
   })
 })

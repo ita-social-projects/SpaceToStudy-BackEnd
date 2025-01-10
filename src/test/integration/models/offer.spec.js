70aa7248afe3abee550307adbe6647c239a96790
luiqor <vohnenna@gmail.com>
2025-01-10 10:31:02 +0200

    add extra fields test

--- src/test/integration/models/offer.spec.js
+++ src/test/integration/models/offer.spec.js
@@ -1,6 +1,23 @@
 const { setupTestServer, stopServer } = require('~/test/setupSafeTest')
 const Offer = require('~/models/offer')
 
+const offerFields = [
+  'price',
+  'proficiencyLevel',
+  'title',
+  'description',
+  'languages',
+  'authorRole',
+  'author',
+  'enrolledUsers',
+  'subject',
+  'category',
+  'status',
+  'FAQ',
+  'createdAt',
+  'updatedAt'
+]
+
 describe('Offer model', () => {
   let server
 
@@ -13,24 +30,58 @@ describe('Offer model', () => {
     await stopServer(server)
   })
 
-  it('should include all required fields', async () => {
+  it('should include all fields', async () => {
     const offers = await Offer.find({})
 
     for (const offer of offers) {
-      expect(offer).toHaveProperty('price')
-      expect(offer).toHaveProperty('proficiencyLevel')
-      expect(offer).toHaveProperty('title')
-      expect(offer).toHaveProperty('description')
-      expect(offer).toHaveProperty('languages')
-      expect(offer).toHaveProperty('authorRole')
-      expect(offer).toHaveProperty('author')
-      expect(offer).toHaveProperty('enrolledUsers')
-      expect(offer).toHaveProperty('subject')
-      expect(offer).toHaveProperty('category')
-      expect(offer).toHaveProperty('status')
-      expect(offer).toHaveProperty('FAQ')
-      expect(offer).toHaveProperty('createdAt')
-      expect(offer).toHaveProperty('updatedAt')
+      offerFields.forEach((field) => {
+        expect(offer).toHaveProperty(field)
+      })
+    }
+  })
+
+  it('should not have any extra fields', async () => {
+    const offers = await Offer.find({})
+
+    for (const offer of offers) {
+      const extraFields = Object.keys(offer).filter((key) => !offerFields.includes(key))
+      expect(extraFields.length).toBe(0)
+    }
+  })
+
+  it('should have valid fields data types', async () => {
+    const offers = await Offer.find({})
+
+    for (const offer of offers) {
+      expect(typeof offer.price).toBe('number')
+      expect(Array.isArray(offer.proficiencyLevel)).toBe(true)
+      expect(typeof offer.title).toBe('string')
+      expect(typeof offer.description).toBe('string')
+      expect(Array.isArray(offer.languages)).toBe(true)
+      expect(typeof offer.authorRole).toBe('string')
+      expect(typeof offer.author).toBe('object')
+      expect(Array.isArray(offer.enrolledUsers)).toBe(true)
+      expect(typeof offer.subject).toBe('object')
+      expect(typeof offer.category).toBe('object')
+      expect(typeof offer.status).toBe('string')
+      expect(Array.isArray(offer.FAQ)).toBe(true)
+      expect(offer.createdAt).toBeInstanceOf(Date)
+      expect(offer.updatedAt).toBeInstanceOf(Date)
+    }
+  })
+
+  it('should have valid items in the FAQ array', async () => {
+    const offers = await Offer.find({})
+
+    for (const offer of offers) {
+      expect(Array.isArray(offer.FAQ)).toBe(true)
+
+      if (offer.FAQ.length > 0) {
+        for (const faq of offer.FAQ) {
+          expect(faq).toHaveProperty('question')
+          expect(faq).toHaveProperty('answer')
+        }
+      }
     }
   })
 })

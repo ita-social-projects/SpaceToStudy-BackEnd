e5ea685220c131fc6e294b43698571dd0ae8c0ba
luiqor <vohnenna@gmail.com>
2025-01-10 10:08:00 +0200

    add all fields test

--- src/test/integration/models/offer.spec.js
+++ src/test/integration/models/offer.spec.js
@@ -1,4 +1,5 @@
 const { setupTestServer, stopServer } = require('~/test/setupSafeTest')
+const Offer = require('~/models/offer')
 
 describe('Offer model', () => {
   let server
@@ -10,5 +11,26 @@ describe('Offer model', () => {
 
   afterAll(async () => {
     await stopServer(server)
+  })
+
+  it('should include all required fields', async () => {
+    const offers = await Offer.find({})
+
+    for (const offer of offers) {
+      expect(offer).toHaveProperty('price')
+      expect(offer).toHaveProperty('proficiencyLevel')
+      expect(offer).toHaveProperty('title')
+      expect(offer).toHaveProperty('description')
+      expect(offer).toHaveProperty('languages')
+      expect(offer).toHaveProperty('authorRole')
+      expect(offer).toHaveProperty('author')
+      expect(offer).toHaveProperty('enrolledUsers')
+      expect(offer).toHaveProperty('subject')
+      expect(offer).toHaveProperty('category')
+      expect(offer).toHaveProperty('status')
+      expect(offer).toHaveProperty('FAQ')
+      expect(offer).toHaveProperty('createdAt')
+      expect(offer).toHaveProperty('updatedAt')
+    }
   })
 })

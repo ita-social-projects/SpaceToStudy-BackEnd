9e34d4a56825377d1792aa45bbb1937f5f4f2b1f
nebby2105 <lopatkaoa@gmail.com>
2025-01-22 01:08:19 +0200

    add migration and tests

---
+++ migrations/20250121143502-remove-cooperations-with-invalid-references.js
@@ -0,0 +1,26 @@
+module.exports = {
+  async up(db) {
+    const mapToId = (items) => {
+      return items.map((item) => item._id)
+    }
+
+    const allOffers = await db.collection('offers').find({}, { _id: true }).toArray()
+    const offerIds = mapToId(allOffers)
+
+    const allUsers = await db.collection('users').find({}, { _id: true }).toArray()
+    const userIds = mapToId(allUsers)
+
+    const invalidCooperations = await db
+      .collection('cooperation')
+      .find({
+        $or: [{ initiator: { $nin: userIds } }, { receiver: { $nin: userIds } }, { offer: { $nin: offerIds } }]
+      })
+      .toArray()
+
+    if (invalidCooperations.length > 0) {
+      await db.collection('cooperation').deleteMany({ _id: { $in: mapToId(invalidCooperations) } })
+    }
+  },
+
+  async down() {}
+}

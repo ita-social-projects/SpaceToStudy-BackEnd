0a3b2eaef5f597e69bde286d34292ca7ab1d457d
nika <vvvveronikaaaa666@gmail.com>
2025-04-15 18:40:50 +0300

    fixed testUserAuth file and package.lock.json

--- src/utils/testUserAuth.js
+++ src/utils/testUserAuth.js
@@ -1,6 +1,8 @@
 const User = require('~/models/user')
 const { hashPassword } = require('~/utils/passwordHelper')
+const dotenv = require('dotenv')
 
+dotenv.config({ path: '.env.test.local' })
 const testUserAuthentication = async (app, testUser = {}) => {
   const qtyOfMandatorySignupFields = 5
   if (Object.keys(testUser).length < qtyOfMandatorySignupFields) {

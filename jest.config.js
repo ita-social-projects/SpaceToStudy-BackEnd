e41e2e48eaa2354162f9afeaeedae6d67569b86b
nika <vvvveronikaaaa666@gmail.com>
2025-04-16 10:12:44 +0300

    configured root for .env.test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src/test'],
+  setupFiles: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/src/$1',
     '@root/(.*)': '<rootDir>/$1'

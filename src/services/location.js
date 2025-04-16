aa00f4890111ad3645b8b67d3a4398cafca1eabb
Iarynovskyi <dimayar20061@gmail.com>
2025-04-16 18:43:35 +0300

    Sorting config variables alphabetically and rename to LOCATION_...

--- src/services/location.js
+++ src/services/location.js
@@ -1,14 +1,14 @@
 const { request } = require('gaxios')
 const {
-  config: { API_URL, API_KEY }
+  config: { LOCATION_API_URL, LOCATION_API_KEY }
 } = require('~/configs/config')
 
 const locationService = {
   getCountries: async () => {
     const res = await request({
-      url: API_URL,
+      url: LOCATION_API_URL,
       headers: {
-        'X-CSCAPI-KEY': API_KEY
+        'X-CSCAPI-KEY': LOCATION_API_KEY
       }
     })
     const countries = res.data.map(({ name, iso2 }) => ({ name, iso2 }))
@@ -18,9 +18,9 @@ const locationService = {
 
   getCities: async (countryCode) => {
     const res = await request({
-      url: `${API_URL}/${countryCode}/cities`,
+      url: `${LOCATION_API_URL}/${countryCode}/cities`,
       headers: {
-        'X-CSCAPI-KEY': API_KEY
+        'X-CSCAPI-KEY': LOCATION_API_KEY
       }
     })
     const cities = res.data.map((city) => city.name)

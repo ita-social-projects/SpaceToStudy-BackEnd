bed271fe747c499bd7fc7e2da919e2d2b90ba2d8
nika <vvvveronikaaaa666@gmail.com>
2025-04-16 11:38:54 +0300

    addded env

--- src/utils/testUserAuth.js
+++ src/utils/testUserAuth.js
@@ -4,17 +4,15 @@ const { hashPassword } = require('~/utils/passwordHelper')
 const testUserAuthentication = async (app, testUser = {}) => {
   const qtyOfMandatorySignupFields = 5
   if (Object.keys(testUser).length < qtyOfMandatorySignupFields) {
-    // Safety check for Role array
     if (!testUser.role) testUser.lastLoginAs = 'student'
     else if (Array.isArray(testUser.role)) testUser.lastLoginAs = testUser.role[0] || 'student'
     else testUser.lastLoginAs = testUser.role
-
     testUser = {
       role: testUser.role ? testUser.role : 'student',
       firstName: 'Tart',
       lastName: 'Drilling',
-      email: 'tartdrilling@gmail.com',
-      password: process.env.DEFAULT_PASSWORD || 'Qwerty123@',
+      email: testUser.email || process.env.TEST_USER_EMAIL,
+      password: testUser.password || process.env.TEST_USER_PASSWORD,
       FAQ: { student: [{ question: 'question1', answer: 'answer1' }] },
       isEmailConfirmed: true,
       lastLoginAs: testUser.lastLoginAs

737fad1344cf0249a5fa104a07b165dee43119f6
nika <vvvveronikaaaa666@gmail.com>
2025-04-17 14:00:37 +0300

    added constants to src/configs/config.js

--- src/utils/testUserAuth.js
+++ src/utils/testUserAuth.js
@@ -1,9 +1,13 @@
 const User = require('~/models/user')
 const { hashPassword } = require('~/utils/passwordHelper')
+const {
+  config: { TEST_USER_EMAIL, TEST_USER_PASSWORD }
+} = require('~/configs/config')
 
 const testUserAuthentication = async (app, testUser = {}) => {
   const qtyOfMandatorySignupFields = 5
   if (Object.keys(testUser).length < qtyOfMandatorySignupFields) {
+    console.log(TEST_USER_EMAIL, TEST_USER_PASSWORD)
     if (!testUser.role) testUser.lastLoginAs = 'student'
     else if (Array.isArray(testUser.role)) testUser.lastLoginAs = testUser.role[0] || 'student'
     else testUser.lastLoginAs = testUser.role
@@ -11,8 +15,8 @@ const testUserAuthentication = async (app, testUser = {}) => {
       role: testUser.role ? testUser.role : 'student',
       firstName: 'Tart',
       lastName: 'Drilling',
-      email: process.env.TEST_USER_EMAIL,
-      password: process.env.TEST_USER_PASSWORD,
+      email: TEST_USER_EMAIL,
+      password: TEST_USER_PASSWORD,
       FAQ: { student: [{ question: 'question1', answer: 'answer1' }] },
       isEmailConfirmed: true,
       lastLoginAs: testUser.lastLoginAs

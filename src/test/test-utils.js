5fb27d59fc99a995c86aa3280fe1d3b2363cfec8
luvthenika <124041223+luvthenika@users.noreply.github.com>
2025-04-23 13:11:31 +0300

    Removed hard-coded creds and fixed CWE-798 Vulnerability  (#1310)
    * removed hard-coded creds
    
    * fixed testUserAuth file and package.lock.json
    
    * configured root for .env.test
    
    * configured mongodb connection
    
    * removed env
    
    * addded env
    
    * configured env
    
    * configured user.spec.js
    
    * added constants to src/configs/config.js
    
    * removed console.log
    
    * added test user and test password

--- src/test/test-utils.js
+++ src/test/test-utils.js
@@ -2,7 +2,6 @@ const testUserAuthentication = require('~/utils/testUserAuth')
 const TokenService = require('~/services/token')
 const checkCategoryExistence = require('~/seed/checkCategoryExistence')
 const Category = require('~/models/category')
-
 const createUser = async (app, testUser = {}) => {
   const accessToken = await testUserAuthentication(app, testUser)
   const decodedToken = TokenService.validateAccessToken(accessToken)
--- src/utils/testUserAuth.js
+++ src/utils/testUserAuth.js
@@ -1,10 +1,12 @@
 const User = require('~/models/user')
 const { hashPassword } = require('~/utils/passwordHelper')
+const {
+  config: { TEST_USER_EMAIL, TEST_USER_PASSWORD }
+} = require('~/configs/config')
 
 const testUserAuthentication = async (app, testUser = {}) => {
   const qtyOfMandatorySignupFields = 5
   if (Object.keys(testUser).length < qtyOfMandatorySignupFields) {
-    // Safety check for Role array
     if (!testUser.role) testUser.lastLoginAs = 'student'
     else if (Array.isArray(testUser.role)) testUser.lastLoginAs = testUser.role[0] || 'student'
     else testUser.lastLoginAs = testUser.role
@@ -13,8 +15,8 @@ const testUserAuthentication = async (app, testUser = {}) => {
       role: testUser.role ? testUser.role : 'student',
       firstName: 'Tart',
       lastName: 'Drilling',
-      email: 'tartdrilling@gmail.com',
-      password: process.env.DEFAULT_PASSWORD || 'Qwerty123@',
+      email: TEST_USER_EMAIL,
+      password: TEST_USER_PASSWORD,
       FAQ: { student: [{ question: 'question1', answer: 'answer1' }] },
       isEmailConfirmed: true,
       lastLoginAs: testUser.lastLoginAs

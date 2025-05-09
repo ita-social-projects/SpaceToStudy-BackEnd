f6dc99e38c9eae92b6902d6f7b8b45ab151de90a
Iarynovskyi <dimayar20061@gmail.com>
2025-05-09 17:46:39 +0300

    now this unused

--- src/utils/toHash.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const { v4: uuidv4 } = require('uuid')
-
-const blobNameToHash = (name) => {
-  const ext = path.extname(name)
-  return `${Date.now()}-${uuidv4()}${ext}`
-}
-
-module.exports = blobNameToHash

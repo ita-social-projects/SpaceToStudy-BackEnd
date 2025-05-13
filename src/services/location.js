db8020ab56dce25cb9ea2aeca269302c17d8d57b
Iryna <ilait@softserveinc.com>
2025-05-13 10:47:34 +0300

    Fixed internal server error #1000

--- src/services/location.js
+++ src/services/location.js
@@ -17,15 +17,29 @@ const locationService = {
   },
 
   getCities: async (countryCode) => {
-    const res = await request({
-      url: `${LOCATION_API_URL}/${countryCode}/cities`,
-      headers: {
-        'X-CSCAPI-KEY': LOCATION_API_KEY
+    if (typeof countryCode !== 'string' || countryCode.length !== 2) {
+      throw {
+        status: 400,
+        message: 'Invalid countryCode. It must be a 2-character string.'
       }
-    })
-    const cities = res.data.map((city) => city.name)
+    }
 
-    return [...new Set(cities)]
+    try {
+      const res = await request({
+        url: `${LOCATION_API_URL}/${countryCode}/cities`,
+        headers: {
+          'X-CSCAPI-KEY': LOCATION_API_KEY
+        }
+      })
+      const cities = res.data.map((city) => city.name)
+
+      return [...new Set(cities)]
+    } catch (error) {
+      throw {
+        status: error.response?.status || 500,
+        message: error.response?.data?.message || 'Internal Server Error'
+      }
+    }
   }
 }
 
